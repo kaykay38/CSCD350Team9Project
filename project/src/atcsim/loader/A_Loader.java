60e0ce4fcfc6ad523726a4fa8f35c373368bb279
coestephen2002 <66713914+coestephen2002@users.noreply.github.com>
2021-11-10 21:59:41 -0800

    Add files via upload

---
+++ project/src/atcsim/loader/A_Loader.java
@@ -0,0 +1,14 @@
+package atcsim.loader;
+
+import atcsim.graphics.view.navigation.OverlayNavigation;
+import atcsim.world.navigation.A_ComponentNavaid;
+
+public abstract class A_Loader {
+	
+	public A_Loader(java.util.Map<String, A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
+		
+		
+	}
+	
+	
+}
---
+++ project/src/atcsim/loader/navaid/LoaderAirway.java
@@ -0,0 +1,24 @@
+package atcsim.loader.navaid;
+
+
+import atcsim.graphics.view.navigation.OverlayNavigation;
+import atcsim.loader.A_Loader;
+import atcsim.world.navigation.A_ComponentNavaid;
+
+public class LoaderAirway extends A_Loader {
+	
+	
+	
+	public LoaderAirway(java.util.Map<String, A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
+		
+		super(navaids, overlay);
+	}
+	
+	
+	
+	public void load(java.util.Scanner scanner) throws java.io.IOException {
+		
+		
+	}
+
+}
