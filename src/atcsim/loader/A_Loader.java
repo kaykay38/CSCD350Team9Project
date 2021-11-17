b34bd301112609d20b97885018894febce7e5dc8
Mia Hunt <miaxyhunt@gmail.com>
2021-11-17 11:16:54 -0800

    Add A_Loader.java

---
+++ src/atcsim/loader/A_Loader.java
@@ -0,0 +1,15 @@
+package atcsim.loader;
+
+import atcsim.graphics.view.navigation.OverlayNavigation;
+import atcsim.world.navigation.A_ComponentNavaid;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+
+public class A_Loader {
+
+    public A_Loader(Map<String,A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
+
+    }
+}
