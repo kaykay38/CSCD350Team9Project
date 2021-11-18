4b0e3164ccac19535cd5efc46373150bf0d747d3
Stephen_Coe <coestephen2002@gmail.com>
2021-11-17 20:13:40 -0800

    First commit

--- src/atcsim/loader/A_Loader.java
+++ src/atcsim/loader/A_Loader.java
@@ -1,15 +1,28 @@
 package atcsim.loader;
 
+import java.util.Map;
+import atcsim.datatype.Altitude;
+import atcsim.datatype.Latitude;
+import atcsim.datatype.Longitude;
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.world.navigation.A_ComponentNavaid;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.Scanner;
-
-public class A_Loader {
-
-    public A_Loader(Map<String,A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
-
-    }
+public abstract class A_Loader {
+	
+	protected String[] splitString;
+	protected Map<String, A_ComponentNavaid<?>> navaids;
+	protected OverlayNavigation overlay;
+	
+	public A_Loader(Map<String, A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
+		
+		this.navaids = navaids;
+		this.overlay = overlay;
+		
+	}
+	
+	protected abstract String readID();
+	protected abstract Latitude readLatitude();
+	protected abstract Longitude readLongitude();
+	protected abstract Altitude readAltitude();
+	
 }
