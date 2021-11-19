f28302ba4ebd907c2fdfa550212ec85ca7b94c70
Mia Hunt <miaxyhunt@gmail.com>
2021-11-18 19:28:37 -0800

    Finish LoaderNDB, LoaderVOR, edit A_Loader

--- src/atcsim/loader/A_Loader.java
+++ src/atcsim/loader/A_Loader.java
@@ -9,52 +9,35 @@ import atcsim.world.navigation.A_ComponentNavaid;
 
 public abstract class A_Loader {
 	
-	protected String[] splitString;
 	protected Map<String, A_ComponentNavaid<?>> navaids;
 	protected OverlayNavigation overlay;
 	
 	public A_Loader(Map<String, A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
-		
 		this.navaids = navaids;
 		this.overlay = overlay;
-		
 	}
 	
-	protected String readID() {
-		
-		return this.splitString[0];
-	}
-	
-	protected abstract String readLatitude();
-	protected abstract String readLongitude();
-	protected abstract String readAltitude();
-	
-	protected Latitude getLatitude(String degreeString, String minutesString, String secondsString) {
+	protected Latitude readLatitude(String degreeString, String minutesString, String secondsString) {
 		  
 		int degrees = Integer.parseInt(degreeString);
 		int minutes = Integer.parseInt(minutesString);
 		double seconds = Double.parseDouble(secondsString);
 		
-		Latitude latitude = new Latitude(degrees, minutes, seconds);
-		return latitude;
+		return new Latitude(degrees, minutes, seconds);
 	}
 	
-	protected Longitude getLongitude(String degreeString, String minutesString, String secondsString) {
+	protected Longitude readLongitude(String degreeString, String minutesString, String secondsString) {
 		
 		int degrees = Integer.parseInt(degreeString);
 		int minutes = Integer.parseInt(degreeString);
 		double seconds = Double.parseDouble(degreeString);
 		
-		Longitude longitude = new Longitude(degrees, minutes, seconds);
-		return longitude;
-	}
+		return new Longitude(degrees, minutes, seconds);
+    }
 	
 	protected Altitude readAltitude(String altitude) {
 		
 		return new Altitude(Double.parseDouble(altitude));
 	}
-	
-		
-	
 	
 }
--- src/atcsim/loader/navaid/LoaderVOR.java
+++ src/atcsim/loader/navaid/LoaderVOR.java
@@ -1,8 +1,14 @@
 package atcsim.loader.navaid;
 
+import atcsim.datatype.Altitude;
+import atcsim.datatype.CoordinateWorld3D;
+import atcsim.datatype.Latitude;
+import atcsim.datatype.Longitude;
+import atcsim.datatype.VHFFrequency;
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
+import atcsim.world.navigation.ComponentNavaidVOR;
 
 import java.io.IOException;
 import java.util.Map;
@@ -10,11 +16,34 @@ import java.util.Scanner;
 
 public class LoaderVOR extends A_Loader {
 
+    private final int ID = 0, VHF_FREQ_MAJ = 1, VHF_FREQ_MIN = 2, LAT_DEG = 3, LAT_MIN = 4, LAT_SEC = 5,
+        LON_DEG = 6, LON_MIN = 7, LON_SEC = 8, ALT = 9;
+
     public LoaderVOR(Map<String, A_ComponentNavaid<?>> navaids, OverlayNavigation overlay) {
         super(navaids, overlay);
     }
 
     public void load(Scanner scanner) throws IOException {
+        String[] splitStr;
 
+        while (scanner.hasNextLine()) {
+            splitStr = scanner.nextLine().split("\s*,\s*");    
+
+            if (splitStr.length > 0) {
+                for (String str : splitStr) {
+                    str.trim();
+                }
+
+                String id = splitStr[ID];
+                VHFFrequency vhfFrequency = new VHFFrequency(Integer.parseInt(splitStr[VHF_FREQ_MAJ]), Integer.parseInt(splitStr[VHF_FREQ_MIN]));
+                Latitude latitude = readLatitude(splitStr[LAT_DEG],splitStr[LAT_MIN],splitStr[LAT_SEC]);
+                Longitude longitude = readLongitude(splitStr[LON_DEG],splitStr[LON_MIN],splitStr[LON_SEC]);
+                Altitude altitude = readAltitude(splitStr[ALT]);
+                CoordinateWorld3D postion = new CoordinateWorld3D(latitude, longitude, altitude);
+
+                this.overlay.addNavaid(new ComponentNavaidVOR(id,postion,vhfFrequency));
+            }
+        }
     }
+
 }
