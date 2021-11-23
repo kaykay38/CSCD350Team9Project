c53cc7b4d2cc6b49f4dfc581f8ea246edfea74c8
Mia Hunt <miaxyhunt@gmail.com>
2021-11-23 12:40:27 -0800

    Add LoaderFix, LoaderNDB Tests

--- test/atcsim/loader/navaid/LoaderFixTests.java
+++ test/atcsim/loader/navaid/LoaderFixTests.java
@@ -1,19 +1,41 @@
 package atcsim.loader.navaid;
 
 import atcsim.graphics.view.navigation.OverlayNavigation;
-import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Map;
+import java.util.HashMap;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertNotNull;
 
 public class LoaderFixTests {
 
+    OverlayNavigation overlay = new OverlayNavigation("1");
+    HashMap<String, A_ComponentNavaid<?>> navaids = new HashMap<>();
+
+    @Test
     public void testLoaderFixConstructor() {
+        LoaderFix loaderFix = new LoaderFix(navaids,overlay);
+        System.out.println("testLoaderFixConstructor: "+ loaderFix);
+        Assertions.assertNotNull(loaderFix);
     }
 
-    public void testLoad() {
-
+    @Test
+    public void testLoad() throws IOException {
+        String resourceName = "definition1.txt";
+        LoaderFix loaderFix = new LoaderFix(navaids,overlay);
+        String id = "fix1";
+        /** definition.txt
+        [NAVAIDS:FIX]
+        fix1, 48,38,31, 116,24,29, 1949
+        */
+        Scanner scanner = new Scanner(new FileInputStream(resourceName));
+        loaderFix.load(scanner);
+        System.out.println("testLoad: "+navaids.get(id));
+        assertNotNull(navaids.get(id));
     }
 }
--- test/atcsim/loader/navaid/LoaderNDBTests.java
+++ test/atcsim/loader/navaid/LoaderNDBTests.java
@@ -1,19 +1,41 @@
 package atcsim.loader.navaid;
 
 import atcsim.graphics.view.navigation.OverlayNavigation;
-import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Map;
+import java.util.HashMap;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertNotNull;
 
 public class LoaderNDBTests {
 
+    OverlayNavigation overlay = new OverlayNavigation("1");
+    HashMap<String, A_ComponentNavaid<?>> navaids = new HashMap<>();
+
+    @Test
     public void testLoaderNDBConstructor() {
+        LoaderNDB loaderNDB = new LoaderNDB(navaids,overlay);
+        System.out.println("testLoaderNDBConstructor: "+ loaderNDB);
+        assertNotNull(loaderNDB);
     }
 
-    public void testLoad() {
-
+    @Test
+    public void testLoad() throws IOException {
+        String resourceName = "definition1.txt";
+        LoaderNDB loaderNDB = new LoaderNDB(navaids, overlay);
+        String id = "fix1";
+        /** definition.txt
+         [NAVAIDS:FIX]
+         fix1, 48,38,31, 116,24,29, 1949
+         */
+        Scanner scanner = new Scanner(new FileInputStream(resourceName));
+        loaderNDB.load(scanner);
+        System.out.println("testLoad: " + navaids.get(id));
+        Assertions.assertNotNull(navaids.get(id));
     }
 }
