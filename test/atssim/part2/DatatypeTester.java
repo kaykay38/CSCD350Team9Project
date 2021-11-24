669f047a3a0ebd9dffe1f298a4d2b0496e3882e2
WesleyJak <jwesley@ewu.edu>
2021-11-24 15:10:34 -0800

    coded testAltitude()

--- test/atssim/part2/DatatypeTester.java
+++ test/atssim/part2/DatatypeTester.java
@@ -1,12 +1,24 @@
 package atssim.part2;
 
+import atcsim.datatype.Altitude;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class DatatypeTester {
 
     @Test
     public void testAltitude() {
+        Altitude altitude1 = new Altitude(1000);
+        Altitude altitude2 = new Altitude(200);
 
+        assertEquals("a1 + a2", 1200.0, altitude1.add_(altitude2).getValue_(), .0001);
+        assertEquals("a2 + a1", 1200.0, altitude2.add_(altitude1).getValue_(), .0001);
+        assertEquals("a1 - a2", 800.0, altitude1.subtract_(altitude2).getValue_(), .0001);
+        assertEquals("a2 - a1", -800.0, altitude2.subtract_(altitude1).getValue_(), .0001);
+        assertEquals("a1 = a1", 0, altitude1.compareTo(altitude1));
+        assertEquals("a1 < a2", -1, altitude2.compareTo(altitude1));
+        assertEquals("a1 > a2", 1, altitude1.compareTo(altitude2));
     }
 
     @Test
