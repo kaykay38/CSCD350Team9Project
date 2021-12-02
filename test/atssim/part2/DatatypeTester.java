2423d5eefb230ffddcffaf2341418900d7f7c0a9
Mia Hunt <miaxyhunt@gmail.com>
2021-12-02 11:53:39 -0800

    Complete testCoordinateWorld3D, add tests from Stephen_Contribution

--- test/atssim/part2/DatatypeTester.java
+++ test/atssim/part2/DatatypeTester.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-
 public class DatatypeTester {
 
     @Test
     public void testAltitude() {
         Altitude a1 = new Altitude(1000);
         Altitude a2 = new Altitude(200);
+
         /* Verify:
         * a1 + a2 is correct. Use getValue_().
         * a2 + a1 is correct.
@@ -34,6 +34,7 @@ public class DatatypeTester {
     public void testAngleNavigational() {
         AngleNavigational a1 = new AngleNavigational(90);
         AngleNavigational a2 = new AngleNavigational(180);
+
         /* Verify:
         * a1 reciprocate is correct.
         * a2 reciprocate is correct.
@@ -44,21 +45,27 @@ public class DatatypeTester {
         AngleNavigational reciprocate2 = a2.reciprocate();
         AngleNavigational interpolate1 = a1.interpolate(a2,new Scaler(.5));
         AngleNavigational interpolate2 = a2.interpolate(a1,new Scaler(.5));
-        assertEquals(0,reciprocate1.compareTo(new AngleNavigational(270)));
-        assertEquals(0,reciprocate2.compareTo(new AngleNavigational(0)));
-        assertEquals(0,interpolate1.compareTo(new AngleNavigational(135)));
-        assertEquals(0,interpolate2.compareTo(new AngleNavigational(315)));
+        assertEquals("reciprocate a1",0,reciprocate1.compareTo(new AngleNavigational(270)));
+        assertEquals("reciprocate a2",0,reciprocate2.compareTo(new AngleNavigational(0)));
+        assertEquals("a1 interpolate a2",0,interpolate1.compareTo(new AngleNavigational(135)));
+        assertEquals("a2 interpolate a1",0,interpolate2.compareTo(new AngleNavigational(315)));
     }
 
     @Test
     public void testAttitudePitch() {
-        AttitudePitch pitch = new AttitudePitch(10);
-        /* Verify:
-        * 0 + p is correct.
-        * 90 + p is correct.
-        * 175 + p is correct.
-        */
+        AttitudePitch p = new AttitudePitch(10);
 
+        /* Verify:
+         * 0 + p is correct.
+         * 90 + p is correct.
+         * 175 + p is correct.
+         */
+        AttitudePitch p0 = new AttitudePitch(0);
+        AttitudePitch p90 = new AttitudePitch(90);
+        AttitudePitch p175 = new AttitudePitch(175);
+        assertEquals("0 + p", 10.0, p0.add_(p).getValue_(), .0001);
+        assertEquals("90 + p", 100.0, p90.add_(p).getValue_(), .0001);
+        assertEquals("175 + p", -175.0, p175.add_(p).getValue_(), .0001);
     }
 
     @Test
@@ -83,14 +90,20 @@ public class DatatypeTester {
 
     @Test
     public void testCourse() {
-        Course course = new Course(10);
+        Course c = new Course(10);
+
         /* Verify:
          * 0 + c is correct.
          * 355 + c is correct.
          * 0 - c is correct.
          * 355 - c is correct.
          */
-
+        Course c0 = new Course(0);
+        Course c355 = new Course(355);
+        assertEquals("0 + c", 10.0, c0.add_(c).getValue_(), .0001);
+        assertEquals("355 + c", 5.0, c355.add_(c).getValue_(), .0001);
+        assertEquals("0 - c", 350.0, c0.subtract_(c).getValue_(), .0001);
+        assertEquals("355 - c", 345.0, c355.subtract_(c).getValue_(), .0001);
     }
 
     @Test
@@ -114,12 +127,13 @@ public class DatatypeTester {
 
     @Test
     public void testCoordinateWorld3D() {
-        //p1 = 49 39 32N, 117 25 30W
-        //p2 = 36 9 58.9N, 115 9 31.3W
         CoordinateWorld p1 = CoordinateWorld.KSFF;
-        CoordinateWorld p2 = new CoordinateWorld(new Latitude(36,9,58.9),new Longitude(115,9,31.3));
-        System.out.println("p1: " + p1);
-        System.out.println("p2: " + p2);
+        CoordinatePolarNavigational bearingToSelf = p1.calculateBearing(p1);
+        CoordinatePolarNavigational bearingKSFF_N = p1.calculateBearing(CoordinateWorld.KSFF_N);
+        CoordinatePolarNavigational bearingKSFF_S = p1.calculateBearing(CoordinateWorld.KSFF_S);
+        CoordinatePolarNavigational bearingKSFF_E = p1.calculateBearing(CoordinateWorld.KSFF_E);
+        CoordinatePolarNavigational bearingKSFF_W = p1.calculateBearing(CoordinateWorld.KSFF_W);
+
         /* Verify:
          * p calculateBearing p is correct for angle. Use getAngle().getValue_().
          * p calculateBearing p is correct for distance. Use getRadiusNauticalMiles().getValue_().
@@ -128,24 +142,15 @@ public class DatatypeTester {
          * p calculateBearing KSFF_E is correct for angle and distance.
          * p calculateBearing KSFF_W is correct for angle and distance.
          */
-        CoordinatePolarNavigational bearing = p1.calculateBearing(p2);
-        // this bearing is correct
-        double expectedBearingP2 = 172.2;
-        double expectedDistanceP2 = 1511;
-
-        double expectedBearingKSFF_N = 0;
-        double expectedDistanceKSFF_N = 0;
-
-        double expectedBearingKSFF_S = 0;
-        double expectedDistanceKSFF_S = 0;
-
-        double expectedBearingKSFF_E = 0;
-        double expectedDistanceKSFF_E = 0;
-
-        double expectedBearingKSFF_W = 0;
-        double expectedDistanceKSFF_W = 0;
-
-        assertEquals("p calculateBearing angle",expectedBearingP2,bearing.getAngle().getValue_(),.1);
-        assertEquals("p calculateBearing distance",expectedDistanceP2,bearing.getRadiusNauticalMiles().getValue_(),.1);
+        assertEquals("p calculateBearing angle",90.0,bearingToSelf.getAngle().getValue_(),.0001);
+        assertEquals("p calculateBearing distance",0.0,bearingToSelf.getRadiusNauticalMiles().getValue_(),.0001);
+        assertEquals("p calculateBearing KSFF_N angle",0.0,bearingKSFF_N.getAngle().getValue_(),.0001);
+        assertEquals("p calculateBearing KSFF_N distance",11.5,bearingKSFF_N.getRadiusNauticalMiles().getValue_(),.0001);
+        assertEquals("p calculateBearing KSFF_S angle",180.0,bearingKSFF_S.getAngle().getValue_(),.0001);
+        assertEquals("p calculateBearing KSFF_S distance",11.5,bearingKSFF_S.getRadiusNauticalMiles().getValue_(),.0001);
+        assertEquals("p calculateBearing KSFF_E angle",90.0,bearingKSFF_E.getAngle().getValue_(),.0001);
+        assertEquals("p calculateBearing KSFF_E distance",11.5,bearingKSFF_E.getRadiusNauticalMiles().getValue_(),.0001);
+        assertEquals("p calculateBearing KSFF_W angle",270.0,bearingKSFF_W.getAngle().getValue_(),.0001);
+        assertEquals("p calculateBearing KSFF_W distance",11.5,bearingKSFF_W.getRadiusNauticalMiles().getValue_(),.0001);
     }
 }
